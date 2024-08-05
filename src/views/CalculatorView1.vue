9f6eb49c5b2e3dc5f38e12beba341b0500d303b1
jelee <waftyann@gmail.com>
2024-08-05 10:09:30 +0900

    계산기1

--- src/views/CalculatorView1.vue
+++ src/views/CalculatorView1.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="">
-    CalculatorView1
+    <h3>CalculatorView1</h3>
+    <input type="text" v-model="num1" @keyup="plusNumbers"> <span> + </span>
+    <input type="text" v-model="num2" @keyup="plusNumbers"> <span> = </span>
+    <span>{{ result }}</span>
   </div>
 </template>
 
@@ -8,68 +11,25 @@
 export default {
   name: 'CalculatorView1',
   props: {
-    // 문자열 타입의 prop 예시
-    //sampleString: {
-    //  type: String,
-    //  default: ''
-    //},
-    // 숫자 타입의 prop 예시
-    //sampleNumber: {
-    //  type: Number,
-    //  default: 0
-    //},
-    // 배열 타입의 prop 예시
-    //sampleArray: {
-    //  type: Array,
-    //  default: () => []
-    //},
-    // 객체 타입의 prop 예시
-    //sampleObject: {
-    //  type: Object,
-    //  default: () => ({})
-    //}
+
   },
   components: {
-    // 추가적으로 사용할 컴포넌트들을 등록합니다.
   },
   data() {
     return {
-      // 컴포넌트의 데이터를 초기화합니다.
+      num1: 0,
+      num2: 0,
+      result: 0,
     };
   },
-  watch: {
-    // sample1() {
-    //   console.log('');
-    // }
-    // 데이터를 감시하고 처리할 로직을 작성합니다.
-  },
-  computed: {
-    // sample2() {
-    //   return '';
-    // }
-    // 필요한 계산된 속성을 정의합니다.
-  },
   methods: {
-    // sample3() {
-    //   return '';
-    // }
-    // 컴포넌트에서 사용할 메서드를 정의합니다.
-  },
-  setup() {
-    // Vue 3 Composition API의 setup 함수에서 추가적인 로직을 처리할 수 있습니다.
-  },
-  created() {
-    // 컴포넌트가 생성될 때 실행될 로직을 작성합니다.
-  },
-  mounted() {
-    // 컴포넌트가 DOM에 마운트된 직후 실행될 로직을 작성합니다.
-  },
-  unmounted() {
-    // 컴포넌트가 파괴되기 전 실행될 로직을 작성합니다.
+    plusNumbers() {
+      this.result = Number(this.num1) + Number(this.num2);
+    }
   }
 };
 </script>
 
 <style scoped>
-/* 스타일을 추가하세요 */
+
 </style>
